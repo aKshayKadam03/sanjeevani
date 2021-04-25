3e128cdcfbca89a456a604a8f5de21446950f868
therealdrtroll <ar1pperk@gmail.com>
2021-04-25 08:18:06 +0530

    final css touches

--- frontend/Components/footer.js
+++ frontend/Components/footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 function footer() {
   return (
     <div className={styles.footer}>
-      <h4>Made with pain</h4>
+      <h4>|| Selfless giving is the art of living ||</h4>
     </div>
   );
 }
